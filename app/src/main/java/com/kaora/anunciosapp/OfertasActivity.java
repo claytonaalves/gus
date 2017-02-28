7e581545179ecee40804f75bdd1c74c2dfd975a7
Clayton A. Alves <clayton.aa@gmail.com>
2017-02-28 11:35:47 -0400

    Adicionado menu toolbar

--- app/src/main/java/com/kaora/anunciosapp/OfertasActivity.java
+++ app/src/main/java/com/kaora/anunciosapp/OfertasActivity.java
@@ -2,7 +2,11 @@ package com.kaora.anunciosapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.kaora.anunciosapp.adapters.OfertaAdapter;
 import com.kaora.anunciosapp.models.Categoria;
@@ -24,6 +28,28 @@ public class OfertasActivity extends AppCompatActivity {
         lvOfertas.setAdapter(adapter);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_categorias, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_tela_inicial:
+                Toast.makeText(this, "menu tela inicial clicado", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            case R.id.action_configuracoes:
+                // abrir activity de configurações
+                break;
+        }
+        return true;
+    }
+
 
     private List<Categoria> carregaCategorias() {
         List<Categoria> categorias = new ArrayList<>();
