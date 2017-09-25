247092d758f697798d484a227aff28b2deeb1f0a
Clayton A. Alves <clayton.aa@gmail.com>
2017-09-25 19:46:34 -0400

    Fixed due date label on ad detail activity

--- app/src/main/java/com/kaora/anunciosapp/activities/PublicacaoActivity.java
+++ app/src/main/java/com/kaora/anunciosapp/activities/PublicacaoActivity.java
@@ -3,6 +3,7 @@ package com.kaora.anunciosapp.activities;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -19,6 +20,8 @@ import com.kaora.anunciosapp.rest.ApiRestAdapter;
 import com.kaora.anunciosapp.utils.DateUtils;
 
 import org.w3c.dom.Text;
+
+import java.text.DateFormat;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -68,7 +71,7 @@ public class PublicacaoActivity extends AppCompatActivity {
         TextView tvDataValidade = (TextView) findViewById(R.id.tvDataValidade);
         Button btLigarPara = (Button) findViewById(R.id.btLigarPara);
 
-        if (publicacao.imagem != "") {
+        if (!(publicacao.imagem==null) && (!publicacao.imagem.equals(""))) {
             SimpleDraweeView imagem = (SimpleDraweeView) findViewById(R.id.imagem_publicacao);
             imagem.setImageURI(ApiRestAdapter.IMAGES_PATH + "/publicacoes/" + publicacao.imagem);
         }
@@ -78,6 +81,12 @@ public class PublicacaoActivity extends AppCompatActivity {
         tvNomeAnunciante.setText(publicacao.anunciante.nomeFantasia);
         tvDataPublicacao.setText(DateUtils.textoDataPublicacao(publicacao.dataPublicacao));
         btLigarPara.setText("Ligar para " + publicacao.anunciante.nomeFantasia + "\n" + publicacao.anunciante.telefone);
+
+        Resources res = this.getResources();
+        DateFormat df = DateFormat.getDateInstance();
+        tvDataValidade.setText(
+            String.format(res.getString(R.string.data_validade), df.format(publicacao.dataValidade))
+        );
     }
 
     public void ligarParaAnunciante(View view) {
