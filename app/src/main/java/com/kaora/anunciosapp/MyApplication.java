4eea33be455b684d6ffdb61825469d86b8e6528a
Clayton A. Alves <clayton.aa@gmail.com>
2017-10-14 18:09:18 -0400

    Added remote error reporting feature

--- app/src/main/java/com/kaora/anunciosapp/MyApplication.java
+++ app/src/main/java/com/kaora/anunciosapp/MyApplication.java
@@ -1,15 +1,30 @@
 package com.kaora.anunciosapp;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 
+import org.acra.ACRA;
+import org.acra.annotation.ReportsCrashes;
+import org.acra.sender.HttpSender;
+
+@ReportsCrashes(
+        formUri = "http://177.221.202.42/cgi-bin/android_error_report",
+        reportType = HttpSender.Type.JSON
+)
 public class MyApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
         Fresco.initialize(this);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        ACRA.init(this);
     }
 
 }
