8a44becd58240d943a5da862bd5b5e9972fea7ff
Clayton A. Alves <clayton.aa@gmail.com>
2018-03-04 14:43:21 -0400

    Scale publication image on rotation

--- app/src/main/java/com/kaora/anunciosapp/activities/PublicationImage.java
+++ app/src/main/java/com/kaora/anunciosapp/activities/PublicationImage.java
@@ -1,13 +1,12 @@
 package com.kaora.anunciosapp.activities;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.kaora.anunciosapp.BuildConfig;
 import com.kaora.anunciosapp.R;
-import com.kaora.anunciosapp.rest.ApiRestAdapter;
 
 public class PublicationImage extends AppCompatActivity {
 
